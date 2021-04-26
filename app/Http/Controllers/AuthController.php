cbe0ede2c5f13a0608560c2b93569e5367bbcbad
Ankush Yadav <ankushyadav9302@gmail.com>
2021-04-27 04:13:21 +0530

    [ADD] Controllers/AuthController.php
    controller to control auth state

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('app');
+        }
+
+        return back()->withErrors([
+            'The provided credentials do not match any records'
+        ])->withInput(
+            $request->except('password')
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
