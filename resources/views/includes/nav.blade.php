6e99fd25612f329f361650f47ebf5ee6e0316fc0
Ankush Yadav <ankushyadav9302@gmail.com>
2021-04-28 02:26:04 +0530

    [REFACTOR] views/includes/nav.blade.php
    minor fixes acc. to previous changes

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -17,7 +17,7 @@
                 {{-- passed as auth status from parent --}}
                 @if ($status)
                     <li class="nav-item">
-                        <a class="nav-link {{ Request::routeIs('profile') ? 'active' : '' }}" href="{{ route('profile', Auth::id()) }}">
+                        <a class="nav-link {{ Request::routeIs('profile') ? 'active' : '' }}" href="{{ route('user.profile') }}">
                             <span class="material-icons left-align">person</span>
                             Profile
                         </a>
