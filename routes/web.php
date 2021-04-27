346fcb69882bb30c6993aa07b59c49d1370423a5
Ankush Yadav <ankushyadav9302@gmail.com>
2021-04-28 02:25:27 +0530

    [UPDATE] routes/web.php
    adds more routes, groups similar ones, ...

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,22 +20,24 @@ Route::get('/', function() {
     return view('welcome');
 })->name('index');
 
+Route::view('/home', 'app');
 Route::view('/app', 'app')->name('app');
 Route::view('/login', 'login')->name('login');
-Route::view('/register', 'app')->name('register');
+Route::view('/register', 'register')->name('register');
 
 Route::get('/get-hash/{pwd}', function($pwd) {
     return Hash::make($pwd);
 });
 
-Route::get('/user/{id}', function($id) {
-    $auth_id = Auth::id();
-    if ($auth_id === (int)$id) {
-        return view('app');
-    }
+Route::name('user.')->prefix('user')->group(function() {
+    Route::post('/add', [UserController::class, 'addUser'])->name('add');
 
-    return redirect()->route('profile', $auth_id);
-})->where('id', '[0-9]+')->name('profile')->middleware('auth');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/profile', [UserController::class, 'getUser'])->name('profile');
+        Route::post('/update', [UserController::class, 'updateUser'])->name('update');
+        Route::delete('/delete', [UserController::class, 'deleteUser'])->name('delete');
+    });
+});
 
 Route::post('/auth',  [AuthController::class, 'authenticate'])->name('authenticate');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
