6b97550382a3095e2d8da5c2653d92f489fcb633
Ankush Yadav <ankushyadav9302@gmail.com>
2021-04-29 04:22:36 +0530

    [UPDATE] Controllers/UserController.php
    completed & working function defifnitions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserController extends Controller
@@ -38,27 +37,80 @@ class UserController extends Controller
     }
 
     public function getUser() {
-        return User::where('id', Auth::id())->get();
+        return \view('profile');
     }
 
     public function updateUser(Request $request) {
-        return 'Update';
-    }
-
-    public function deleteUser(Request $request) {
-        $validator = $request->validate([
-            'password' => 'required | min:4'
+        $validator = $request->validateWithBag('updateAcc', [
+            'name' => 'required | regex:/^[a-zA-Z\s]*$/ | min:3',
+            'email' => 'required | email'
         ]);
 
-        if (! \Hash::check(\Hash::make($request->input('password')), Auth::user()->password)) {
-            return redirect()->route('app')->with([
-                'status' => 'danger',
-                'message' => 'Incorrect Password!'
+        try {
+            $user = User::where('id', Auth::id())->first();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->save();
+
+            return redirect()->back()->with([
+                'status' => 'success',
+                'message' => 'Profile updated'
+            ]);
+        } catch (\Exception $e) {
+            return back()->withErrors([
+                'Internal server error!',
+                'Email already taken!'
+            ]);
+        }
+    }
+
+    public function changePassword(Request $request) {
+        $validator = $request->validateWithBag('changePwd', [
+            'password' => 'required | min:4',
+            'new_password' => 'required | confirmed | min:4'
+        ]);
+
+        if (! \Hash::check($request->input('password'), Auth::user()->password)) {
+            return back()->withErrors([
+                '<strong class="text-danger">Incorrect Password!</strong> Failed to change.'
+            ]);
+        }
+
+        if (\Hash::check($request->input('new_password'), Auth::user()->password)) {
+            return back()->withErrors([
+                '<strong class="text-danger">New password same as previous!</strong> Unable to update.'
             ]);
         }
 
         try {
-            $user = User::where('id', Auth::id());
+            $user = User::where('id', Auth::id())->first();
+            $user->password = \Hash::make($request->input('new_password'));
+            $user->save();
+
+            return redirect()->back()->with([
+                'status' => 'success',
+                'message' => 'Password successfully changed'
+            ]);
+        } catch (\Exception $e) {
+            return back()->withErrors([
+                'Internal server error!'
+            ]);
+        }
+    }
+
+    public function deleteUser(Request $request) {
+        $validator = $request->validateWithBag('deleteAcc', [
+            'password' => 'required | min:4'
+        ]);
+
+        if (! \Hash::check($request->input('password'), Auth::user()->password)) {
+            return back()->withErrors([
+                '<strong class="text-danger">Incorrect Password!</strong> Failed to delete account.'
+            ]);
+        }
+
+        try {
+            $user = User::where('id', Auth::id())->first();
             $user->delete();
 
             return redirect()->route('logout');
