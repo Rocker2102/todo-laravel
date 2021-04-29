e91e106f9d36649cef1aaec7cf2b2e3ca1a1ccc4
Ankush Yadav <ankushyadav9302@gmail.com>
2021-04-30 03:12:59 +0530

    [UPDATE] routes/web.php
    groups routes

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,37 @@ use App\Http\Controllers\UserController;
 |
 */
 
+Route::redirect('/home', '/app', 301);
+
+Route::get('/login', function() {
+    return redirect()->route('app.login');
+})->name('login');
+
 Route::get('/', function() {
     return view('welcome');
 })->name('index');
 
-Route::view('/home', 'app');
-Route::view('/app', 'app')->name('app');
-Route::view('/login', 'login')->name('login');
-Route::view('/register', 'register')->name('register');
+/* grouped routes */
+
+Route::name('app.')->prefix('app')->group(function() {
+    Route::get('/profile', function() {
+        return redirect()->route('user.profile');
+    })->name('profile');
+
+    Route::view('/', 'app')->name('home');
+    Route::view('/login', 'login')->name('login');
+    Route::view('/register', 'register')->name('register');
+});
 
 Route::name('user.')->prefix('user')->group(function() {
     Route::post('/add', [UserController::class, 'addUser'])->name('add');
+    Route::post('/auth',  [AuthController::class, 'authenticate'])->name('authenticate');
 
     Route::middleware(['auth'])->group(function () {
+        Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
         Route::get('/profile', [UserController::class, 'getUser'])->name('profile');
         Route::post('/update', [UserController::class, 'updateUser'])->name('update');
         Route::post('/change-password', [UserController::class, 'changePassword'])->name('change_pwd');
         Route::delete('/delete', [UserController::class, 'deleteUser'])->name('delete');
     });
 });
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::post('/auth',  [AuthController::class, 'authenticate'])->name('authenticate');
