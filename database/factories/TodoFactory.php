69e722bf16f15fb63279b098e2976b5339e6eb86
Ankush Yadav <ankushyadav9302@gmail.com>
2021-05-02 07:01:25 +0530

    [ADD] database/factories/TodoFactory
    adds factory class for todo's

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Todo;
+
+class TodoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $users = \App\Models\User::all()->pluck('id')->toArray();
+
+        return [
+            'user_id' => $this->faker->randomElements($users),
+            'title' => $this->faker->word(\mt_rand(3, 8)),
+            'description' => $this->faker->paragraph()
+        ];
+    }
+}
