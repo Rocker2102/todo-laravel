1dcd1f640a0cc2ed21d68d745a012c031ca02676
Ankush Yadav <ankushyadav9302@gmail.com>
2021-06-04 02:56:01 +0530

    [UPDATE] Models/Todo.php
    adds default casts

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -14,6 +14,17 @@ class Todo extends Model
 
     protected $table = 'todos';
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'due_date' => 'datetime:d-m-Y H:i:s',
+        'created_at' => 'datetime:d-m-Y H:i:s',
+        'updated_at' => 'datetime:d-m-Y H:i:s'
+    ];
+
     public function user() {
         return $this->belongsTo(User::class)->withDefault();
     }
