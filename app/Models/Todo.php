4265ace6cd2acc38f8820aedd92bcf932a96da91
Ankush Yadav <ankushyadav9302@gmail.com>
2021-06-04 04:07:28 +0530

    [UPDATE] Models/Todo.php
    adds custom mutators, removes casts

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,19 +14,28 @@ class Todo extends Model
     use HasFactory, SoftDeletes;
 
     protected $table = 'todos';
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'due_date' => 'datetime:d-m-Y H:i:s',
-        'created_at' => 'datetime:d-m-Y H:i:s',
-        'updated_at' => 'datetime:d-m-Y H:i:s'
-    ];
+    protected $dateFormat = 'd-m-Y H:i:s';
 
     public function user() {
         return $this->belongsTo(User::class)->withDefault();
+    }
+
+    /* custom mutators */
+    public function getCreatedAtAttribute($value) {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone('Asia/Kolkata')
+            ->format($this->dateFormat);
+    }
+
+    public function getUpdatedAtAttribute($value) {
+        return Carbon::createFromTimestamp(strtotime($value))
+            ->timezone('Asia/Kolkata')
+            ->format($this->dateFormat);
+    }
+
+    public function getDueDateAttribute($value) {
+        return isset($value) ? Carbon::createFromTimestamp(strtotime($value))
+            ->timezone('Asia/Kolkata')
+            ->format($this->dateFormat) : null;
     }
 }
